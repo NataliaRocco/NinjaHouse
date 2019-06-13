081805dae4ea07aaa3c7862faff4ad79b62ccb6a
NataliaRocco <37155536+NataliaRocco@users.noreply.github.com>
2019-06-13 17:25:47 -0300

    Update tarefas_steps.rb

--- ninjahouse/mark7/features/step_definitions/tarefas_steps.rb
+++ ninjahouse/mark7/features/step_definitions/tarefas_steps.rb
@@ -2,6 +2,14 @@ Dado('que {string} é a minha nova tarefa') do |nova_tarefa|
   @nova_tarefa = {nome: nova_tarefa, data: nil, tags: [] }
 end
 
+Dado("que {string} é a minha nova tarefa dinamica") do |nova_tarefa|
+  @nova_tarefa = { 
+    nome: "#{nova_tarefa} - #{Faker::Lorem.characters(10)}",
+    data: nil,
+     tags: [] 
+    }
+end
+
 Dado('devo finalizar esta tarefa até {string}') do |data_tarefa|
   @nova_tarefa[:data] = data_tarefa
 end
