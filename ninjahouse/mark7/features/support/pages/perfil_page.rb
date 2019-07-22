7c0d6aefa3adb4ffe5892c41a6af49ed3f77ba4d
NataliaRocco <37155536+NataliaRocco@users.noreply.github.com>
2019-07-21 21:31:19 -0300

    Add files via upload

--- ninjahouse/mark7/features/support/pages/perfil_page.rb
+++ ninjahouse/mark7/features/support/pages/perfil_page.rb
@@ -2,10 +2,10 @@ class PerfilPage
 
     include Capybara::DSL
 
-    def completa(empresa, cargo)
-        find('input[name$=company]').set empresa
+    def completa(perfil)
+        find('input[name$=company]').set perfil[:empresa]
         combo_cargo = find('select[name$=job]')
-        combo_cargo.find('option', text: cargo).select_option   # dentro do elemento find('select [name$=job]') procura uma option
+        combo_cargo.find('option', text: perfil[:cargo]).select_option   # dentro do elemento find('select [name$=job]') procura uma option
         click_button 'Salvar' #'click_button' do tipo button
     end
     
